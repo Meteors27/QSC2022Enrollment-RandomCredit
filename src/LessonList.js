22645d21347045d4c08a00f31c071af94326c34d
Meteors27 <williamxia27@qq.com>
2022-10-04 02:34:17 +0800

    除了cookie有效期内自动登录(bonus)其他功能均实现

--- src/LessonList.js
+++ src/LessonList.js
@@ -3,29 +3,35 @@ import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
+
 
 const LessonList = ({lessons}) => {
     return ( 
         <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        <nav aria-label="main mailbox folders">
-            <List>
-                {lessons.map((lesson) => (
-                    <div className="lesson" key={lesson.name}>
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                            <ListItemText primary={lesson.name} />
-                            </ListItemButton>
-                        </ListItem>
-                    </div>
-                ))}
-            </List>
-        </nav>
-        </Box>    
+            <nav aria-label="main mailbox folders">
+                <List>
+                    <ListItem lesson-head>
+                        <ListItemButton>
+                            <ListItemText primary='课程名称'/>
+                            <ListItemText secondary='绩点' align='right'/>
+                        </ListItemButton>
+                    </ListItem>
+                    <Divider />
+                    {lessons.map((lesson) => (
+                        <div className="lesson" key={lesson.name}>
+                            <ListItem lesson>
+                                <ListItemButton>
+                                    <ListItemText primary={lesson.name} />
+                                    <ListItemText secondary={lesson.credit} align='right' />
+                                </ListItemButton>
+                            </ListItem>
+                        </div>
+                    ))}
+                </List>
+            </nav>
+        </Box>
     );
 }
  
--- src/Query.js
+++ src/Query.js
@@ -1,13 +1,14 @@
 import { useHistory } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import LessonList from './LessonList';
-import BasicList from './BasicList';
+
+import Button from '@mui/material/Button';
+import { Container } from '@mui/system';
 
 export default function Query() {
     const history = useHistory();
     const [lessons, setLessons] = useState(null);
-
-    useEffect(() => {
+    const refresh = () => {
         fetch('http://localhost:9999/credit', {
             method: 'GET',
             credentials: 'include',
@@ -16,17 +17,34 @@ export default function Query() {
             },
         })
             .then(res => {
+                if (!res.ok) {
+                    alert("登录过期，请重新登录");
+                    history.go(-1);
+                }
                 return res.json();
             })
             .then(data => {
                 console.log(data);
                 setLessons(data.data);
             })
-    },[]);
+    }
+    useEffect(refresh,[]);
 
     return(
         <div className='lesson-list'>
-            {lessons && <LessonList lessons={lessons}/>}
+            <Container maxWidth='xs'>
+                {lessons && <LessonList lessons={lessons}/>}
+                <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={refresh}
+                >
+                刷新
+                </Button>
+            </Container>
+            
         </div>
     );
 }
